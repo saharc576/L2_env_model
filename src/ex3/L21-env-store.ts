431284a8e709483bbbbdeeb0127ac04c293e6be8
sahar <sahar576cohen@gmail.com>
2021-05-04 14:06:49 +0300

    set ecal works, must delete printing

--- src/ex3/L21-env-store.ts
+++ src/ex3/L21-env-store.ts
@@ -1,96 +1,119 @@
-import { add, map, reduce, zipWith } from "ramda";
-import { Value } from './L21-value-store';
-import { Result, makeFailure, makeOk, bind, either } from "../shared/result";
-import { rest } from "../shared/list";
+import { add, isEmpty, map, reduce, zipWith } from 'ramda'
+import { Value } from './L21-value-store'
+import { Result, makeFailure, makeOk, bind, either } from '../shared/result'
+import { rest } from '../shared/list'
 
 // ========================================================
 // Box datatype
 // Encapsulate mutation in a single type.
-type Box<T> = T[];
-const makeBox = <T>(x: T): Box<T> => ([x]);
-const unbox = <T>(b: Box<T>): T => b[0];
-const setBox = <T>(b: Box<T>, v: T): void => { b[0] = v; return; }
+type Box<T> = T[]
+const makeBox = <T>(x: T): Box<T> => [x]
+const unbox = <T>(b: Box<T>): T => b[0]
+const setBox = <T>(b: Box<T>, v: T): void => {
+  b[0] = v
+  return
+}
 
 // ========================================================
 // Store datatype
 export interface Store {
-    tag: "Store";
-    vals: Box<Value>[];
+  tag: 'Store'
+  vals: Box<Box<Value>[]>
 }
 
-export const isStore = (x: any): x is Store => x.tag === "Store";
-export const makeEmptyStore = (): Store => ({tag: "Store", vals: []});
+export const isStore = (x: any): x is Store => x.tag === 'Store'
+export const makeEmptyStore = (): Store => ({ tag: 'Store', vals: makeBox([]) })
 export const theStore: Store = makeEmptyStore();
 
 export const extendStore = (s: Store, val: Value): Store => {
-    const newStoreVals = s.vals.length === 0 ? [makeBox(val)]: s.vals.concat(makeBox(val));
-    return {tag: "Store", vals: newStoreVals};
+  const valsUnboxed = unbox(s.vals);
+  s.vals = isEmpty(valsUnboxed) ? makeBox(makeBox([val])) : makeBox(valsUnboxed.concat(makeBox([val])));
+  console.log("\n\nextend store -- new store is %j\n\n", s);       // TODO: delete
+
+  return s;
 }
-    
-export const applyStore = (store: Store, address: number): Result<Value> =>
-    store.vals.length > address ? makeOk(unbox(store.vals[address])) : makeFailure("address is illegal");
 
-    
-export const setStore = (store: Store, address: number, val: Value): void => 
-    setBox(store.vals[address], val);
+export const applyStore = (store: Store, address: number): Result<Value> => 
+  unbox(store.vals).length >= address
+  ? makeOk(unbox(unbox(store.vals)[address]))
+  : makeFailure('address is illegal')
 
+
+
+export const setStore = (store: Store, address: number, val: Value): void => {
+  console.log("\n\nset store -- OLD store[address] is %j", (unbox(store.vals))[address]);       // TODO: delete
+  setBox(unbox(store.vals)[address], val)
+  console.log("\nset store -- new store[address] is %j\n\n", (unbox(store.vals))[address]);      // TODO: delete
+  return;
+}
 
 // ========================================================
 // Environment data type
 // export type Env = EmptyEnv | ExtEnv;
-export type Env = GlobalEnv | ExtEnv;
+export type Env = GlobalEnv | ExtEnv
 
 interface GlobalEnv {
-    tag: "GlobalEnv";
-    vars: Box<string[]>;
-    addresses: Box<number[]>
+  tag: 'GlobalEnv'
+  vars: Box<string[]>
+  addresses: Box<number[]>
 }
 
 export interface ExtEnv {
-    tag: "ExtEnv";
-    vars: string[];
-    addresses: number[];
-    nextEnv: Env;
+  tag: 'ExtEnv'
+  vars: string[]
+  addresses: number[]
+  nextEnv: Env
 }
 
-const makeGlobalEnv = (): GlobalEnv =>
-    ({tag: "GlobalEnv", vars: makeBox([]), addresses:makeBox([])});
+const makeGlobalEnv = (): GlobalEnv => ({
+  tag: 'GlobalEnv',
+  vars: makeBox([]),
+  addresses: makeBox([]),
+})
 
-export const isGlobalEnv = (x: any): x is GlobalEnv => x.tag === "GlobalEnv";
+export const isGlobalEnv = (x: any): x is GlobalEnv => x.tag === 'GlobalEnv'
 
 // There is a single mutable value in the type Global-env
-export const theGlobalEnv = makeGlobalEnv();
+export const theGlobalEnv = makeGlobalEnv()
 
-export const makeExtEnv = (vs: string[], addresses: number[], env: Env): ExtEnv =>
-    ({tag: "ExtEnv", vars: vs, addresses: addresses, nextEnv: env});
+export const makeExtEnv = (vs: string[], addresses: number[], env: Env): ExtEnv => ({
+  tag: 'ExtEnv',
+  vars: vs,
+  addresses: addresses,
+  nextEnv: env,
+})
 
-const isExtEnv = (x: any): x is ExtEnv => x.tag === "ExtEnv";
+const isExtEnv = (x: any): x is ExtEnv => x.tag === 'ExtEnv'
 
-export const isEnv = (x: any): x is Env => isGlobalEnv(x) || isExtEnv(x);
+export const isEnv = (x: any): x is Env => isGlobalEnv(x) || isExtEnv(x)
 
 // Apply-env
-export const applyEnv = (env: Env, v: string): Result<number> =>
-    isGlobalEnv(env) ? applyGlobalEnv(env, v) :
-    applyExtEnv(env, v);
-
-const applyGlobalEnv = (env: GlobalEnv, v: string): Result<number> => {
-
-    const index = findIndex(env.vars, v, 0);
-    return index >= 0 ? makeOk(unbox(env.addresses[index])) : makeFailure("value doesn't exist");
-
+export const applyEnv = (env: Env, v: string): Result<number> => {
+  const ret =   isGlobalEnv(env) ? applyGlobalEnv(env, v) : applyExtEnv(env, v)
+  return ret;
 }
 
-const findIndex = (vars: Box<string[]>, v: string, pos: number): number => 
-    vars.length > 0 ? unbox(unbox(vars)) === v ? pos 
-                    : findIndex(rest(vars), v, pos + 1)
-                    : -1
-
-
+const applyGlobalEnv = (env: GlobalEnv, v: string): Result<number> => 
+  unbox(env.vars).includes(v) ? makeOk(unbox(env.addresses[unbox(env.vars).indexOf(v)])) : makeFailure("value doesn't exist");
+  
 export const globalEnvAddBinding = (v: string, addr: number): void => {
-    theGlobalEnv.addresses.push(makeBox(addr));
-    theGlobalEnv.vars.push(makeBox(v));
+  setBox(theGlobalEnv.addresses, unbox(theGlobalEnv.addresses).concat(addr))
+  setBox(theGlobalEnv.vars, unbox(theGlobalEnv.vars).concat(v))
 }
 
 const applyExtEnv = (env: ExtEnv, v: string): Result<number> =>
-    env.vars.includes(v) ? makeOk(env.addresses[env.vars.indexOf(v)]) :
-    applyEnv(env.nextEnv, v);
+  env.vars.includes(v) ? makeOk(env.addresses[env.vars.indexOf(v)]) : applyEnv(env.nextEnv, v)
+
+export const applyEnvStore = (env: Env, v: string): Result<number> =>
+  isGlobalEnv(env) ? applyGlobalEnvStore(env, v) :
+  isExtEnv(env) ? applyExtEnvStore(env, v) :
+  env;
+
+
+
+const applyGlobalEnvStore = (ge: GlobalEnv, v: string): Result<number> =>
+  bind(applyGlobalEnv(ge, v), (address: number) => makeOk(address));
+
+const applyExtEnvStore = (env: ExtEnv, v: string): Result<number> =>
+  bind(applyExtEnv(env, v),  (address: number) => makeOk(address));
+
--- src/ex3/L21-eval-store.ts
+++ src/ex3/L21-eval-store.ts
@@ -5,27 +5,30 @@
 import { map, reduce, repeat, zipWith } from "ramda";
 import { isBoolExp, isCExp, isLitExp, isNumExp, isPrimOp, isStrExp, isVarRef,
          isAppExp, isDefineExp, isIfExp, isLetExp, isProcExp, Binding, VarDecl, CExp, Exp, IfExp, LetExp, ProcExp, Program,
-         parseL21Exp, DefineExp} from "./L21-ast";
-import { applyEnv, makeExtEnv, Env, Store, setStore, extendStore, ExtEnv, applyEnvStore, theGlobalEnv, globalEnvAddBinding, theStore } from "./L21-env-store";
+         parseL21Exp, DefineExp, isSetExp, SetExp} from "./L21-ast";
+import { applyEnv, makeExtEnv, Env, Store, setStore, extendStore, ExtEnv, applyEnvStore, theGlobalEnv, globalEnvAddBinding, theStore, isGlobalEnv, applyStore } from "./L21-env-store";
 import { isClosure, makeClosure, Closure, Value } from "./L21-value-store";
 import { applyPrimitive } from "./evalPrimitive-store";
 import { first, rest, isEmpty } from "../shared/list";
 import { Result, bind, safe2, mapResult, makeFailure, makeOk } from "../shared/result";
 import { parse as p } from "../shared/parser";
+import { makeBox, unbox } from "../shared/box";
 
 // ========================================================
 // Eval functions
 
-const applicativeEval = (exp: CExp, env: Env): Result<Value> =>
+const applicativeEval = (exp: CExp, env: Env): Result<Value> => 
+    
     isNumExp(exp) ? makeOk(exp.val) :
     isBoolExp(exp) ? makeOk(exp.val) :
     isStrExp(exp) ? makeOk(exp.val) :
     isPrimOp(exp) ? makeOk(exp) :
-    isVarRef(exp) ? ...§ :
+    isVarRef(exp) ? bind(applyEnv(env, exp.var), (address: number) => applyStore(theStore, address))  :
     isLitExp(exp) ? makeOk(exp.val as Value) :
     isIfExp(exp) ? evalIf(exp, env) :
     isProcExp(exp) ? evalProc(exp, env) :
     isLetExp(exp) ? evalLet(exp, env) :
+    isSetExp(exp) ? evalSet(exp, env) :
     isAppExp(exp) ? safe2((proc: Value, args: Value[]) => applyProcedure(proc, args))
                         (applicativeEval(exp.rator, env), mapResult((rand: CExp) => applicativeEval(rand, env), exp.rands)) :
     exp;
@@ -37,27 +40,45 @@ const evalIf = (exp: IfExp, env: Env): Result<Value> =>
     bind(applicativeEval(exp.test, env),
          (test: Value) => isTrueValue(test) ? applicativeEval(exp.then, env) : applicativeEval(exp.alt, env));
 
-const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>
-    makeOk(makeClosure(exp.args, exp.body, env));
+const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>{
+    // console.log("evaluating proc, env is %j\n proc is %j", env, exp);       // TODO: delete
+    return makeOk(makeClosure(exp.args, exp.body, env));
+
+}
 
 // KEY: This procedure does NOT have an env parameter.
 //      Instead we use the env of the closure.
-const applyProcedure = (proc: Value, args: Value[]): Result<Value> =>
-    isPrimOp(proc) ? applyPrimitive(proc, args) :
+const applyProcedure = (proc: Value, args: Value[]): Result<Value> => {
+    // console.log("applying procedure, proc is %j\n args are %j", proc, args);       // TODO: delete
+    return  isPrimOp(proc) ? applyPrimitive(proc, args) :
     isClosure(proc) ? applyClosure(proc, args) :
     makeFailure(`Bad procedure ${JSON.stringify(proc)}`);
+}
+   
 
 const applyClosure = (proc: Closure, args: Value[]): Result<Value> => {
+    // console.log("\n\napply closure -- proc closure is %j", proc);       // TODO: delete
+    // console.log("\napply closure -- args (Value[]) is %j", args);       // TODO: delete
     const vars = map((v: VarDecl) => v.var, proc.params);
-    const addresses: number[] = ...
+    map((v: VarDecl) => v.var == "x" ? console.log("=======+++++++++++=================") : console.log(""), proc.params)
+    const addresses: number[] = map((arg: Value) => (unbox(extendStore(theStore,arg).vals)).length -1, args)
     const newEnv: ExtEnv = makeExtEnv(vars, addresses, proc.env)
+    console.log("\napply closure - new env is: %j\n  addresses are %j\n  vars are %j\n\n", newEnv.tag, addresses, vars);       // TODO: delete
+    // console.log("the store is %j\n\n", theStore);       // TODO: delete
+
     return evalSequence(proc.body, newEnv);
 }
 
 // Evaluate a sequence of expressions (in a program)
-export const evalSequence = (seq: Exp[], env: Env): Result<Value> =>
-    isEmpty(seq) ? makeFailure("Empty program") :
+export const evalSequence = (seq: Exp[], env: Env): Result<Value> => {
+    // console.log("\n\neval sequence -- seq = Exp[] is : %j\n", seq);       // TODO: delete
+    // console.log("eval sequence -- first(seq) is : %j\n", first(seq));       // TODO: delete
+    // console.log("eval sequence -- rest(seq) is : %j\n\n", rest(seq));       // TODO: delete
+
+    return isEmpty(seq) ? makeFailure("Empty program") :
     evalCExps(first(seq), rest(seq), env);
+}
+    
     
 const evalCExps = (first: Exp, rest: Exp[], env: Env): Result<Value> =>
     isDefineExp(first) ? evalDefineExps(first, rest) :
@@ -65,13 +86,28 @@ const evalCExps = (first: Exp, rest: Exp[], env: Env): Result<Value> =>
     isCExp(first) ? bind(applicativeEval(first, env), _ => evalSequence(rest, env)) :
     first;
 
-const evalDefineExps = (def: DefineExp, exps: Exp[]): Result<Value> =>
-    // complete
+
+const evalDefineExps = (def: DefineExp, exps: Exp[]): Result<Value> => {return makeFailure("failure in define")}
+//     const valsResult = applicativeEval(def.val, theGlobalEnv);
+//     const addresses  =
+    
+// }
+//     // bind(applicativeEval(def.val, theGlobalEnv),
+//     //         (val: Value) => { applyEnv(theGlobalEnv, val.)
+//     //                             globalEnvAddBinding(def.var.var, rhs);
+//     //                             return evalSequence(exps, theGlobalEnv); });
+
 
 // Main program
 // L2-BOX @@ Use GE instead of empty-env
-export const evalProgram = (program: Program): Result<Value> =>
-    evalSequence(program.exps, theGlobalEnv);
+export const evalProgram = (program: Program): Result<Value> => {
+    // console.log("\n\neval program -- before evaluation: %j", program);       // TODO: delete
+    // console.log("\neval program -- program.exps: %j\n\n", program.exps);       // TODO: delete
+
+    const a = evalSequence(program.exps, theGlobalEnv);
+    return a;
+
+}
 
 export const evalParse = (s: string): Result<Value> =>
     bind(bind(p(s), parseL21Exp), (exp: Exp) => evalSequence([exp], theGlobalEnv));
@@ -82,10 +118,27 @@ const evalLet = (exp: LetExp, env: Env): Result<Value> => {
     const vals = mapResult((v: CExp) => applicativeEval(v, env), map((b: Binding) => b.val, exp.bindings));
     const vars = map((b: Binding) => b.var.var, exp.bindings);
 
-    
     return bind(vals, (vals: Value[]) => {
-        const addresses = ...
+        const addresses = map((val: Value) => (unbox(extendStore(theStore,val).vals)).length -1, vals)
         const newEnv = makeExtEnv(vars, addresses, env)
         return evalSequence(exp.body, newEnv);
     })
 }
+
+const evalSet = (exp: SetExp, env: Env): Result<void> => {
+    console.log("\n\n evalSet --- the last val is %j \n\n", (unbox(theStore.vals))[0]) // TODO: delete
+    console.log("\n\n evalSet --- the env tag is %j \n\n", env.tag) // TODO: delete
+    console.log("\n\n evalSet --- the addresses are %j \n\n", env.addresses) // TODO: delete
+    console.log("\n\n evalSet --- the vars are %j \n\n", env.vars) // TODO: delete
+    console.log("\n\n =================== \n\n") // TODO: delete
+
+    const a=   safe2((val: Value, address: number) => makeOk(setStore(theStore, address, val)))
+    (applicativeEval(exp.val, env), applyEnvStore(env, exp.var.var));
+    console.log("\n\n evalSet --- the last val is %j \n\n", (unbox(theStore.vals))[0]) // TODO: delete
+    console.log("\n\n evalSet --- the env tag is %j \n\n", env.tag) // TODO: delete
+    console.log("\n\n evalSet --- the addresses are %j \n\n", env.addresses) // TODO: delete
+    console.log("\n\n evalSet --- the vars are %j \n\n", env.vars) // TODO: delete
+    return a;
+
+}
+  
